d9f0c858de3e07bf2cd94165c68032d7efa81d5e
Xuan <leeyitsuen0617@gmail.com>
2023-10-16 15:06:53 +0800

    modify

--- 1016/index4.py
+++ 1016/index4.py
@@ -9,7 +9,6 @@ from PIL import Image, ImageTk
 class Window(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.geometry("300x250+800+300")
         self.title("Lines")
         #self.configure(background='#ED784A')        
 
@@ -17,6 +16,25 @@ class MyFrame(tk.LabelFrame):
     def __init__(self,master,title,**kwargs):
         super().__init__(master,text=title,**kwargs)
         self.pack(expand=1, fill='both', padx=10, pady=10)
+
+        #標題
+        heading = ttk.Label(self,text="會員登入",font=('Helvetica',20),foreground='green')
+        heading.grid(column=0,row=0,columnspan=2)
+
+        username_label = ttk.Label(self,text="使用者名稱:",font=('Helvetica',10))
+        username_label.grid(column=0, row=1,pady=10,padx=(10,1))
+
+        username_entry = ttk.Entry(self)
+        username_entry.grid(column=1, row=1,padx=(0,10))
+
+        password_label = ttk.Label(self,text="密碼:",font=('Helvetica',10))
+        password_label.grid(column=0, row=2,sticky=tk.E, pady=10)
+        
+        password_entry = ttk.Entry(self,show="*")
+        password_entry.grid(column=1, row=2,padx=(0,10))
+
+        login_button = ttk.Button(self,text="登入")
+        login_button.grid(column=1,row=3,sticky=tk.E,padx=(0,10),pady=(0,20))
 
     def choiced(self):
         print(self.aligement.get())
@@ -28,7 +46,6 @@ def main():
     '''
     window = Window()
     myFrame = MyFrame(window,title="對齊方式")
-    s = ttk.Style()
     
     window.mainloop()
 
