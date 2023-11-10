f598a254be576ae93b2a13d542c7a8765c08d77e
Xuan <leeyitsuen0617@gmail.com>
2023-11-10 15:35:02 +0800

    modify

--- youbike_render_1108_1110/datasource.py
+++ youbike_render_1108_1110/datasource.py
@@ -63,3 +63,21 @@ def update_render_data()->None:
     for item in data:
         __insert_data(conn,values=[item['sna'],item['sarea'],item['mday'],item['ar'],item['tot'],item['sbi'],item['bemp']])
     conn.close()
+
+def lastest_datetime_data()->list[tuple]:
+    conn = psycopg2.connect(database=pw.DATABASE,
+                            user=pw.USER, 
+                            password=pw.PASSWORD, host=pw.HOST, 
+                            port="5432")
+    cursor = conn.cursor()              
+    sql = '''
+        SELECT 站點名稱, MAX(更新時間) AS 更新時間, 行政區, 地址, 總車輛數, 可借, 可還
+        FROM 台北市youbike
+        GROUP BY 站點名稱, 行政區, 地址, 總車輛數, 可借, 可還
+    '''
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return rows
--- youbike_render_1108_1110/index.py
+++ youbike_render_1108_1110/index.py
@@ -1,22 +1,72 @@
+import datasource
+import psycopg2
+import password as pw
 import tkinter as tk
 from tkinter import ttk
-import datasource
+from tkinter import messagebox
+from youbikeTreeView import YoubikeTreeView
+from threading import Timer
 
 
 class Window(tk.Tk):
-    pass
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        #---------建立介面------------------------
+        #print(datasource.lastest_datetime_data())
+        topFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1)
+        tk.Label(topFrame,text="台北市youbike及時資料",font=("arial", 20), bg="#333333", fg='#ffffff',padx=10,pady=10).pack(padx=20,pady=20)
+        topFrame.pack(pady=30)
+        #---------------------------------------
+
+        #---------------建立treeView---------------
+        bottomFrame = tk.Frame(self)
+        
+        self.youbikeTreeView = YoubikeTreeView(bottomFrame,show="headings",
+                                               columns=('sna','mday','sarea','ar','tot','sbi','bemp'),
+                                               height=20)
+        self.youbikeTreeView.pack(side='left')
+        vsb = ttk.Scrollbar(bottomFrame, orient="vertical", command=self.youbikeTreeView.yview)
+        vsb.pack(side='left',fill='y')
+        self.youbikeTreeView.configure(yscrollcommand=vsb.set)
+        bottomFrame.pack(pady=(0,30),padx=20)
+        #-------------------------------------------
+
+
 
 def main():
     def update_data(w:Window)->None:
-        datasource.update_render_data()
+        #-----------更新treeView資料---------------
+        #-----------必需先顯示treeView資料,再更新資料,因為更新資料的時間太長----------
+        
+        try:
+            datasource.updata_render_data()
+            #pass
+        except Exception:
+            messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
+            #window.destroy()
 
-        w.after(5*60*1000,update_data,w) #每5分鐘執行,w參數傳遞到def update_data(w)
+        lastest_data = datasource.lastest_datetime_data()
+        w.youbikeTreeView.update_content(lastest_data)
+        
+        
+
+        #w.after(5*60*1000,update_data,w) #每隔5分鐘
+        t = Timer(5*60, update_data,args=(window,))
+        t.start()
 
     window = Window()
     window.title('台北市youbike2.0')
+    #window.geometry('600x300')
     window.resizable(width=False,height=False)
-    update_data(window)
+    lastest_data = datasource.lastest_datetime_data()
+    window.youbikeTreeView.update_content(lastest_data)
+    #window.after(1000,update_data,window) 
+    t = Timer(5, update_data,args=(window,))
+    t.start()         
     window.mainloop()
+    
 
-if __name__ == '__main__':
+    
+
+if __name__ == "__main__":
     main()
