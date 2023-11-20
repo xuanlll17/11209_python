daa350a089a16d36086e66b8e3b610ec2eb4f649
Xuan <leeyitsuen0617@gmail.com>
2023-11-20 03:57:59 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    print('debug')      # 出現在終端機
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
