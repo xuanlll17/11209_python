026b0c5fcac06ad210d969994233618ff2d6827e
Xuan <leeyitsuen0617@gmail.com>
2023-12-04 03:50:00 +0000

    modify

--- flask_dash/flask_dash_web/main.py
+++ flask_dash/flask_dash_web/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello!</p>"
+def index():
+    return render_template("index.html")
