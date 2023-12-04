dda2de04a098aceee7818ea6fe1d683532eaeaf5
Xuan <leeyitsuen0617@gmail.com>
2023-12-04 08:15:21 +0000

    modify

--- flask_dash/dash_file/dash_app1.py
+++ flask_dash/dash_file/dash_app1.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash(__name__)
+dash1 = Dash(__name__)
 
-app.layout = html.Div([
+dash1.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
@@ -21,4 +21,4 @@ def update_graph(value):
     return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    dash1.run(debug=True)
--- flask_dash/main.py
+++ flask_dash/main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template,url_for
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from dash_file.dash_app1 import dash1
+from werkzeug.serving import run_simple
 
 app = Flask(__name__)
+application = DispatcherMiddleware(
+    app,
+    {"/dash/app1": dash1.server},
+)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
