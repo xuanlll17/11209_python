5ecf498dd9d091c73e802cc33974b91dbfd264b6
HH <wlhs1726@gmail.com>
2023-12-09 02:52:22 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -6,12 +6,13 @@ import pandas as pd
 dash2 = Dash(requests_pathname_prefix="/dash/app2/", external_stylesheets=[dbc.themes.BOOTSTRAP])  #建立時一定要加路徑
 
 #property layout
-dash2.layout = html.Div(children=[  #children可打可不打
-    html.H1("Dash H1"),
-    html.P("段落1"),
-    html.P("段落2"),
-
-])
+dash2.layout = html.Div(
+    [html.H1("Bootstrap Layout"),
+     html.P("這是段落1"),
+     html.P("這是段落2")],   #要放多個,所以用list代替  #一定要把layout内容對齊
+    className="container-lg",
+    style={'backgroundColor':'#666'}   #使用駝峰式命名法
+    )
 
 
 #if __name__ == '__main__':
