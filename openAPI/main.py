9a0d573a069e3d27026179263fb45468eb55f0c4
HH <wlhs1726@gmail.com>
2024-02-19 15:20:37 +0800

    modify

--- openAPI/main.py
+++ openAPI/main.py
@@ -4,12 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
+# server提供了兩個get @app.get("/") @app.get("/items/{item_id}")
+@app.get("/") 
 def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None): # q -> query(不在路徑裡)(選填)
     return {"item_id": item_id, "q": q}
+
+# 執行main.py -> 終端機 uvicorn main(main.py):app(main.py裡面的app) --reload(開發環境下須打)
